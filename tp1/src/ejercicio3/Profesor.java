e6641e58a0c1c778c9c88fc2fc126152c073c9ab
Angelo <angelorma7@gmail.com>
2024-05-15 01:27:31 -0300

    v3

---
+++ tp1/src/ejercicio3/Profesor.java
@@ -0,0 +1,12 @@
+package ejercicio3;
+
+public class Profesor {
+
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String catedra;
+    private String facultad;
+
+
+}
